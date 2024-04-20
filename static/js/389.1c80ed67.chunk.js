e7646e336d6c1b096b5dba7cb0242e22c3a517fa
Maryna-Korbet <97749149+Maryna-Korbet@users.noreply.github.com>
2024-04-20 02:02:19 +0000

    Deploying to gh-pages from @ Maryna-Korbet/campervan-rental@9df59a7af4ca2adf9f0f853f894911088557b753 🚀

--- static/js/389.1c80ed67.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcampervan_rental=self.webpackChunkcampervan_rental||[]).push([[389],{4389:function(n,r,e){e.r(r),e.d(r,{default:function(){return u}});var a=e(4718),t=e(184);function u(){return(0,t.jsx)(a.Z,{color:"primary",sx:{mt:1}})}}}]);
-//# sourceMappingURL=389.1c80ed67.chunk.js.map
